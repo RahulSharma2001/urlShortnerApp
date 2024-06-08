7e02a44637023484c4bc4848601482c6b660f97f
Rahul sharma <sharma.rahul0579@gmail.com>
2024-06-08 11:56:33 +0530

    URL shortner Application complete

---
+++ index.js
@@ -0,0 +1,46 @@
+import express from "express";
+import { nanoid } from "nanoid";
+import bodyParser from "body-parser";
+import fs from "fs";
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.render("short", { shortUrl: "", isActive: false });
+});
+
+app.post("/api/v1/shortUrl", (req, res) => {
+  const id = nanoid(8);
+  const longUrl = req.body.longUrl;
+  console.log(longUrl);
+  let data = {};
+  try {
+    if (fs.existsSync("urlData.json"))
+      data = JSON.parse(fs.readFileSync("urlData.json", "utf8"));
+
+    data[id] = longUrl;
+
+    fs.writeFileSync("urlData.json", JSON.stringify(data));
+
+    res.render("short", {
+      shortUrl: `http://localhost:5000/${id}`,
+      isActive: true,
+    });
+  } catch (e) {
+    res.render("short", { shortUrl: e, isActive: true });
+  }
+});
+
+app.get("/:shortUrl", (req, res) => {
+  const data = JSON.parse(fs.readFileSync("urlData.json", "utf-8"));
+  const longUrl = data[req.params.shortUrl];
+  res.redirect(longUrl);
+});
+
+app.listen(PORT, () => console.log(`Server up and running ${PORT}`));
